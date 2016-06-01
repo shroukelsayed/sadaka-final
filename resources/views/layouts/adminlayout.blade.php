384a206c32d86c0bde7a6913129c30eaa7e7a16d
osamasabry <osamasabry14@gmail.com>
2016-06-01 22:56:42 +0200

    modify by osama sabry

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -316,15 +316,29 @@
             <li class="header">MAIN NAVIGATION</li>
             <li class="active treeview">
               <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+                <i class="glyphicon glyphicon-home"></i> <span>Home</span> 
               </a>
-              <ul class="treeview-menu">
-                <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-              </ul>
+
             </li>
             <li>
-              <a href="Admin/pages/widgets.html">
-                <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
+              <a href="{{URL::to('/charities')}}">
+                <i class="fa fa-th"></i> <span>All Cases</span> <small class="label pull-right bg-green">new</small>
+              </a>
+            </li>
+
+            <li>
+              <a href="{{URL::to('/people/create')}}">
+                <i class="fa fa-th"></i> <span>Add Cases</span> <small class="label pull-right bg-green">new</small>
+              </a>
+            </li>
+            <li>
+             <a href="{{URL::to('/compaigns')}}">
+                <i class="fa fa-th"></i> <span>All Compaign</span> <small class="label pull-right bg-green">new</small>
+              </a>
+            </li>
+            <li>
+            <a href="{{URL::to('/compaigns/create')}}">
+                <i class="fa fa-th"></i> <span>Add Compaign</span> <small class="label pull-right bg-green">new</small>
               </a>
             </li>
             
@@ -398,7 +412,7 @@
             <small>Control panel</small>
           </h1>
           <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="{{URL::to('/charities')}}"><i class="fa fa-dashboard"></i> Home</a></li>
             <li class="active">Dashboard</li>
           </ol>
         </section>
@@ -411,6 +425,7 @@
         </div>
       <!-- /.content-wrapper -->
       </div>
+       <div class="control-sidebar-bg"></div>
     </div>
 
       <!-- /.container -->
